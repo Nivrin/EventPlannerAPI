478f077206a155636a3bb3f62976e91687bd8ba6
nivrimn <nivrimn@gmail.com>
2024-03-16 18:17:06 +0200

    added logger route

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
--- app/main.py
+++ app/main.py
@@ -3,18 +3,23 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.responses import FileResponse
 from app.auth.auth import create_access_token, authenticate_user
 from app.database.database import get_db, create_database  # Import create_database function
 from app.routes import events, users
 import uvicorn
 from sqlalchemy.orm import Session
-from app.tasks.reminder_tasks import celery_
+# from app.tasks.reminder_tasks import celery_
 from app.utils.logger import  setup_logging
 import logging
+import os
+
 
 app = FastAPI(debug=True)
+
 setup_logging()
 logging.info('Application started')
+LOG_FILE_PATH = "./data_and_logs/logs/app.log"
 
 create_database()
 
@@ -46,6 +51,14 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         )
     access_token = create_access_token(user.username)
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/logs")
+async def get_logs():
+    if not os.path.exists(LOG_FILE_PATH):
+        raise HTTPException(status_code=404, detail="Log file not found")
+
+    return FileResponse(LOG_FILE_PATH)
 
 
 @app.exception_handler(RequestValidationError)
